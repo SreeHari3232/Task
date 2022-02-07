34c2d60609f27a7f6d3a3f8aee5510e2d44d2ca6
SreeHari <sreehari400.sh@gmail.com>
2022-02-07 23:14:43 +0530

    Tasks

--- three.py
+++ three.py
@@ -2,14 +2,14 @@ n=3
 for i in range(n-1,-1,-1):
     for j in range(i):
         print(end="--")
-    for j in range(n-1,i,-1):
+    for k in range(n-1,i,-1):
         print("*",end="-")
-    for k in range(i,n):
-        if k != n-1:
+    for l in range(i,n):
+        if l != n-1:
             print("*",end="-")
         else:
             print("*",end="")
-    for l in range(2*i):
+    for m in range(2*i):
         print(end="-")
     print()
 for i in range(1,n):
